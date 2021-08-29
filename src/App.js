59a99979b1b50cc5c85024a44a2f5d98c4f9e175
george <heorhii.makeiev@gmail.com>
2021-08-30 02:24:04 +0300

    reakt7Lection

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import {useReducer} from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const reducer = (state, action) => {
+    if (action.obj === '1' && action.diya === '+') {
+        return {...state, a: state.a++}
+    } else if (action.obj === '1' && action.diya === '-') {
+        return {...state, a: state.a--}
+    } else if (action.obj === '2' && action.diya === '+') {
+        return {...state, b: state.b++}
+    } else if (action.obj === '2' && action.diya === '-') {
+        return {...state, b: state.b--}
+    }
 }
 
-export default App;
+export default function App() {
+    let [{a, b}, dispatch] = useReducer(reducer, {a: 0, b: 0});
+    return (
+        <div>
+            <h1>state 1 - {a}</h1>
+            <button onClick={() => dispatch({obj: '1', diya: '+'})}>inc</button>
+            <button onClick={() => dispatch({obj: '1', diya: '-'})}>dec</button>
+            <h1>state 1 - {b}</h1>
+            <button onClick={() => dispatch({obj: '2', diya: '+'})}>inc</button>
+            <button onClick={() => dispatch({obj: '2', diya: '-'})}>dec</button>
+        </div>
+    );
+}
