c70ee8477ff5e427d1365dd9ec53fffae53c6bd9
george <heorhii.makeiev@gmail.com>
2021-08-30 21:34:06 +0300

    reakt7classwork

--- src/App.js
+++ src/App.js
@@ -1,16 +1,25 @@
+// стоврити об'єкт {a:0,b:0,c:0}
+// Стоврити 6 кнопок.
+//     по дві на кожен з параметрів об'єкту.
+// одна кнопка збільшує значення , інша зменшує
+// логіку реалізувати через reducer
+
 import {useReducer} from "react";
 import reducer from "./reducers/Reducer";
 
 export default function App() {
-    let [{a, b}, dispatch] = useReducer(reducer, {a: 0, b: 0});
+    let [{a, b, c}, dispatch] = useReducer(reducer, {a: 0, b: 0, c: 0});
     return (
         <div>
             <h1>state 1 - {a}</h1>
             <button onClick={() => dispatch({obj: '1', diya: '+'})}>inc</button>
             <button onClick={() => dispatch({obj: '1', diya: '-'})}>dec</button>
-            <h1>state 1 - {b}</h1>
+            <h1>state 2 - {b}</h1>
             <button onClick={() => dispatch({obj: '2', diya: '+'})}>inc</button>
             <button onClick={() => dispatch({obj: '2', diya: '-'})}>dec</button>
+            <h1>state 3 - {c}</h1>
+            <button onClick={() => dispatch({obj: '3', diya: '+'})}>inc</button>
+            <button onClick={() => dispatch({obj: '3', diya: '-'})}>dec</button>
         </div>
     );
 }
--- src/reducers/Reducer.js
+++ src/reducers/Reducer.js
@@ -7,6 +7,10 @@ const reducer = (state, action) => {
         return {...state, b: state.b++}
     } else if (action.obj === '2' && action.diya === '-') {
         return {...state, b: state.b--}
+    } else if (action.obj === '3' && action.diya === '+') {
+        return {...state, c: state.c++}
+    } else if (action.obj === '3' && action.diya === '-') {
+        return {...state, c: state.c--}
     }
 };
 
