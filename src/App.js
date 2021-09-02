fa220b30329bdc1781c92032fd269d4bf4670414
george <heorhii.makeiev@gmail.com>
2021-09-03 02:54:49 +0300

    reakt7lection

--- src/App.js
+++ src/App.js
@@ -1,19 +1,20 @@
-import {useEffect, useReducer} from "react";
+import { useEffect, useReducer } from "react";
 import reducer from "./reducers/reducer";
-import {getUsers} from "./services/user.api.service";
+import { getUsers } from "./services/user.api.service";
 import Users from "./components/Users";
 
 export default function App() {
 
-    let [{users, posts}, dispatch] = useReducer(reducer,
+    const [{users, posts}, dispatch] = useReducer(reducer,
         {users:[], posts: []});
     useEffect(async() => {
         let users = await getUsers();
-        dispatch({type: 'GET_USERS', payload: users});
+        dispatch({ type: 'GET_USERS', payload: users });
     }, []);
     return (
         <div>
             <Users items={users}/>
         </div>
     );
+
 }
--- src/services/post.api.service.js
+++ src/services/post.api.service.js
@@ -1,4 +1,4 @@
-let url1 = 'http://jsonplaceholder.typicode.com/posts'
+const url1 = 'http://jsonplaceholder.typicode.com/posts'
 const getPosts = () => fetch(url1)
         .then(value => value.json())
 
