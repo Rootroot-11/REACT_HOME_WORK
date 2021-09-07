9fd9b7c22be5a8cbcc0afa31f9cdd1885c382c25
george <heorhii.makeiev@gmail.com>
2021-09-08 00:14:01 +0300

    reakt_project

--- src/components/Movie_Details/MovieDetails.js
+++ src/components/Movie_Details/MovieDetails.js
@@ -1,27 +1,23 @@
-import {useEffect, useState} from "react";
+
 import {useLocation} from "react-router";
 import {discoverMovie, getMovieByid} from "../../services/service.api/movieService";
-import {Route, Router} from "react-router-dom";
 
-export default function MovieDetails ({ match: { params:{ id } } })  {
-    // const {state} = useLocation();
-    const [movieByID, setMovieByid] = useState({});
-    useEffect(()=>{
-        getMovieByid(id).then(value => setMovieByid({...value.data}));
-    },[id]);
 
-    document.body.style.backgroundImage = `url(https://image.tmdb.org/t/p/w500${movieByID.backdrop_path})`
-    return(
-        <Router>
+export default function MovieDetails({match: {params: {id}}}) {
+    const {state} = useLocation();
+    // const [movieByID, setMovieByid] = useState({});
+    // useEffect(() => {
+    //     getMovieByid(id).then(value => setMovieByid({...value.data}));
+    // }, [id]);
+    //
+    // document.body.style.backgroundImage = `url(https://image.tmdb.org/t/p/w500${movieByID.backdrop_path})`
+    return (
+
         <div>
-
-            <Route exact path={'/movies/'} component={MovieDetails}/>
-            <h1>{movieByID.title}</h1>
-            <img src={`https://image.tmdb.org/t/p/w500${movieByID.poster_path}`} alt='Poster'/>
-            <p>FJDIVJCVJ</p>
+            {JSON.stringify(state)}
 
         </div>
-        </Router>
+
     );
 }
 
