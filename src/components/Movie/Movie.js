b250b89e6c22fbab370c2cca45b81142dde7f961
george <heorhii.makeiev@gmail.com>
2021-09-08 00:17:39 +0300

    reakt_project

--- src/components/Movie/Movie.js
+++ src/components/Movie/Movie.js
@@ -1,21 +1,20 @@
 import {useSelector} from "react-redux";
-import {Link} from "react-router-dom";
+import {Link, Route} from "react-router-dom";
+import MovieDetails from "../Movie_Details/MovieDetails";
 
-export const Movie = ({value}) => {
+export const Movie = ({item}) => {
 
     const {genres} = useSelector(({genresReducer}) => genresReducer)
 
-    const genresForOneMovie = value.genre_ids.map(value => genres?.find(genre => genre.id === value));
+    const genresForOneMovie = item.genre_ids.map(value => genres?.find(genre => genre.id === value));
 
 
     return (
         <div className={'Movie'}>
             <div className={'Movie-box'}>
-                <div className={'box'}>
-                    <div className={'p_item'}>
-                        <Link to={{pathname: '/movies/'}}><h3>{value.title}</h3></Link>
-                    </div>
-                </div>
+                {item.id} {item.title} - <Link to={{pathname: '/posts/' + item.id, state: item}}>post static details</Link>
+
+                <Route path={'/posts/' + item.id} component={MovieDetails}/>
             </div>
         </div>
     )
