ad74c1428d7ff82d6ea6659e20a6b3e2c40de86e
george <heorhii.makeiev@gmail.com>
2021-09-08 00:34:39 +0300

    reakt_project

--- src/components/Movie_Details/MovieDetails.js
+++ src/components/Movie_Details/MovieDetails.js
@@ -1,17 +1,16 @@
-import {useLocation} from "react-router";
-
+import { useLocation } from "react-router";
 
 export default function MovieDetails({match: {params: {id}}}) {
 
     const {state} = useLocation();
 
     return (
-
         <div>
+
             {JSON.stringify(state)}
 
         </div>
-
     );
+
 }
 
