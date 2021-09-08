404b03af7bae87e0b13b70c21c946fb6547bb9e2
george <heorhii.makeiev@gmail.com>
2021-09-08 11:40:14 +0300

    reakt_project

--- src/components/MoviesPage/Movies.js
+++ src/components/MoviesPage/Movies.js
@@ -1,29 +1,30 @@
-import { useState } from "react";
+import {useState} from "react";
 import {useDispatch, useSelector} from "react-redux";
 import {Movie} from "../Movie/Movie";
-import {Route} from "react-router-dom";
+import {Route, Router} from "react-router-dom";
 import MovieDetails from "../Movie_Details/MovieDetails";
 
 export default function Movies(props) {
-    const { match: { url }, history } = props;
+    const {match: {url}, history} = props;
 }
-    const [currentPage, setCurrentPage] = useState(1);
-    const [fetching, setFetching] = useState(true)
+const [currentPage, setCurrentPage] = useState(1);
+const [fetching, setFetching] = useState(true)
 
-    let { users } = useSelector(({rootReducer}) => rootReducer);
-    let { genres } = useSelector(({genresReducer}) => genresReducer);
-    const dispatch = useDispatch();
+let {users} = useSelector(({rootReducer}) => rootReducer);
+let {genres} = useSelector(({genresReducer}) => genresReducer);
+const dispatch = useDispatch();
 
 
-    return (
+return (
 
-        <div>
+    <div>
 
-            {
-                users.map(value => <Movie key={value.id} value={value}/>)
-            }
-            {  <Route exact path={/:id`} component={MovieDetails}/>}
+        {
+            users.map(value => <Movie key={value.id} value={value}/>)
+        }
+        <Route exact path={`${url}/:id`} component={MovieDetails}/>
 
-                </div>
-    );
+    </div>
+
+);
 }
