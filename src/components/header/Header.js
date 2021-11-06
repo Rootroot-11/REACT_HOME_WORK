599898b17fb4be157cadc1f4ec49a8e50150c06a
george <heorhii.makeiev@gmail.com>
2021-11-06 17:49:32 +0300

    React project

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,14 +1,19 @@
 import './Header.css'
 
-export const Header = () => {
-    return (
-
-        <header className={'header'}>
-            <button className={'anColor'}>Change color</button>
-            <button className={'btn'}>Search movie</button>
-            <button className={'auth'}>Authorization</button>
-
-
-        </header>
-    )
-}
+// export const Header = () => {
+//     return (
+//
+//         // <header className={'header'}>
+//         //     <button className={'anColor'}>Change color</button>
+//         //     <button className={'btn'}>Search movie</button>
+//         //     <button className={'auth'}>Authorization</button>
+//         //
+//         //
+//         //
+//         // </header>
+//
+//     // let filter_select = document.getElementsById('filter');
+//
+//     // let item = document.getElementsByClassName()
+//     )
+// }
